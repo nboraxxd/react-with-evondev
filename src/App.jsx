17ceb7496b52cacbad92a6830f56a1117037cc6a
bora <nhatbaoxxd@gmail.com>
2023-05-31 15:12:57 +0700

    theme in styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import './App.scss'
 import CardList from './components/card/CardList'
 import { GlobalStyles } from './components/GlobalStyle'
 
+const theme = {
+  colors: {
+    purple: '#7d6aff',
+  },
+}
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <CardList />
-    </div>
+    </ThemeProvider>
   )
 }
 
